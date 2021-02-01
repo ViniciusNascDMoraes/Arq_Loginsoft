fdcc7fdb977138db14cf1dd7495e42277422da1d
Vinicius <viniciuspiracaia@hotmail.com>
2021-02-01 15:34:57 -0300

    Update main.dart

--- ProjetoFlutter/OlaMundo/lib/main.dart
+++ ProjetoFlutter/OlaMundo/lib/main.dart
@@ -5,6 +5,13 @@ import 'package:flutter/material.dart';
 
 main(){
 
-  runApp(Container());
+  runApp(Container(
+    child: Center(
+      child: Text('Fluterando', 
+      textDirection: TextDirection.ltr, 
+      style: TextStyle(color: Colors.green, fontSize: 10)),
+    )
+
+  ));
 
 }
