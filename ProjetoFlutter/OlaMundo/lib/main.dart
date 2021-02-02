621b5fbf623fb06fbcde243187fcef5530503024
Vinicius <viniciuspiracaia@hotmail.com>
2021-02-02 15:57:59 -0300

    Update main.dart

--- ProjetoFlutter/OlaMundo/lib/main.dart
+++ ProjetoFlutter/OlaMundo/lib/main.dart
@@ -1,17 +1,18 @@
-
-
-
 import 'package:flutter/material.dart';
 
-main(){
+main() {
+  runApp(AppWidget());
+}
 
-  runApp(Container(
-    child: Center(
-      child: Text('Fluterando', 
-      textDirection: TextDirection.ltr, 
-      style: TextStyle(color: Colors.green, fontSize: 10)),
-    )
-
-  ));
-
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Center(
+       child: Text('Fluterando',
+       textDirection: TextDirection.ltr,
+       style: TextStyle(color: Colors.green, fontSize: 10)),
+        )
+     )
+  }
 }
